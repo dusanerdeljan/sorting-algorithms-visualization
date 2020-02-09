815576361f408c4358e43d8d5447d318f949d4d2
dusanerdeljan <dusanerdeljan99@gmail.com>
2020-02-09 11:26:47 +0100

    Implemented sorting algorithm selection system.

--- SortingAlgorithms/Main.cpp
+++ SortingAlgorithms/Main.cpp
@@ -15,7 +15,7 @@
 
 struct Button
 {
-	enum class State {CHECKED, UNCHECKED};
+	enum class State {UNCHECKED = 0, CHECKED};
 
 	std::string text;
 	uint32_t width;
@@ -23,8 +23,11 @@ struct Button
 	uint32_t x;
 	uint32_t y;
 	State state;
-	Button(uint32_t x, uint32_t y, uint32_t w, uint32_t h, std::string text, Button::State state = Button::State::UNCHECKED)
-		: text(text), width(w), height(h), x(x), y(y), state(state) { }
+	SortingAlgorithmFactory::Algorithm algorithm;
+	Button(uint32_t x, uint32_t y, uint32_t w, uint32_t h, std::string text, 
+		SortingAlgorithmFactory::Algorithm algorithm = SortingAlgorithmFactory::Algorithm::MERGE_SORT, 
+		Button::State state = Button::State::UNCHECKED)
+		: text(text), width(w), height(h), x(x), y(y), state(state), algorithm(algorithm) { }
 	bool PointInsideButton(uint32_t x, uint32_t y) const
 	{
 		return (x >= this->x && x <= this->x + width && y >= this->y && y <= this->y + height);
@@ -42,19 +45,39 @@ private:
 	std::vector<Animation> m_AnimationVector;
 	Button m_NewArrayButton;
 	Button m_SortButton;
+	Button m_BubbleSortButton;
+	Button m_SelectionSortButton;
+	Button m_InsertionSortButton;
+	Button m_MergeSortButton;
+	Button m_HeapSortButton;
+	Button m_QuickSortButton;
+	Button* m_SelectedSortButton;
 	std::vector<Button*> m_Buttons;
 	int m_First = -1;
 	int m_Second = -1;
 	bool m_ShouldDrawArray = true;
-	SortingAlgorithmFactory::Algorithm m_SortingAlgorithm = SortingAlgorithmFactory::Algorithm::HEAP_SORT;
+	SortingAlgorithmFactory::Algorithm m_SortingAlgorithm = SortingAlgorithmFactory::Algorithm::MERGE_SORT;
 	ApplicationState m_State = ApplicationState::ARRAY_GENERATION;
 public:
 	SortingAlgorithms()
 		: m_NewArrayButton(10, 10, 160, 20, "Generate new array"),
-		m_SortButton(180, 10, 160, 20, "Sort!")
+		m_SortButton(180, 10, 160, 20, "Sort!"),
+		m_BubbleSortButton(350, 10, 160, 20, "Bubble sort", SortingAlgorithmFactory::Algorithm::BUBBLE_SORT),
+		m_SelectionSortButton(520, 10, 160, 20, "Selection sort", SortingAlgorithmFactory::Algorithm::SELECTION_SORT),
+		m_InsertionSortButton(690, 10, 160, 20, "Insertion sort", SortingAlgorithmFactory::Algorithm::INSERTION_SORT),
+		m_MergeSortButton(860, 10, 160, 20, "Merge sort", SortingAlgorithmFactory::Algorithm::MERGE_SORT, Button::State::CHECKED),
+		m_HeapSortButton(1030, 10, 160, 20, "Heap sort", SortingAlgorithmFactory::Algorithm::HEAP_SORT),
+		m_QuickSortButton(1200, 10, 160, 20, "Quick sort", SortingAlgorithmFactory::Algorithm::QUICK_SORT)
 	{
 		m_Buttons.push_back(&m_NewArrayButton);
 		m_Buttons.push_back(&m_SortButton);
+		m_Buttons.push_back(&m_BubbleSortButton);
+		m_Buttons.push_back(&m_SelectionSortButton);
+		m_Buttons.push_back(&m_InsertionSortButton);
+		m_Buttons.push_back(&m_MergeSortButton);
+		m_Buttons.push_back(&m_HeapSortButton);
+		m_Buttons.push_back(&m_QuickSortButton);
+		m_SelectedSortButton = &m_MergeSortButton;
 		sAppName = "Sorting algorithms visualization";
 	}
 public:
@@ -74,7 +97,7 @@ public:
 		if (m_State == ApplicationState::ARRAY_SORTED)
 		{
 			if (m_ShouldDrawArray)
-				DrawNumbers(olc::GREEN);
+				DrawNumbers(olc::DARK_GREEN);
 			return true;
 		}
 		if (m_First != -1 && m_Second != -1)
@@ -113,38 +136,48 @@ private:
 	{
 		if (GetMouse(0).bPressed)
 		{
-			for (const Button* btn : m_Buttons)
+			for (Button* btn : m_Buttons)
 			{
 				if (btn->PointInsideButton(GetMouseX(), GetMouseY()))
 				{
 					if (btn == &m_NewArrayButton)
-					{
-						if (m_State == ApplicationState::SORTING_ANIMATION)
-							return;
-						GenerateArray();
-						Clear(olc::BLACK);
-						m_ShouldDrawArray = true;
-						m_State = ApplicationState::ARRAY_GENERATION;
-					}
+						GenerateArrayButtonAction();
 					else if (btn == &m_SortButton)
+						SortButtonAction();
+					else
 					{
-						if (m_State != ApplicationState::ARRAY_GENERATION)
-							return;
-						m_Sort = SortingAlgorithmFactory::GetSortingAlgorithm(m_SortingAlgorithm);
-						m_State = ApplicationState::SORTING_ANIMATION;
-						m_ShouldDrawArray = true;
-						std::vector<int> copy(m_NumberArray);
-						m_AnimationVector = m_Sort->SortNumbers(copy);
-						m_AnimationIterator = m_AnimationVector.begin();
+						m_SelectedSortButton->state = Button::State::UNCHECKED;
+						btn->state = Button::State::CHECKED;
+						m_SelectedSortButton = btn;
+						m_SortingAlgorithm = btn->algorithm;
 					}
 				}
 			}
 		}
 	}
+	void GenerateArrayButtonAction()
+	{
+		if (m_State == ApplicationState::SORTING_ANIMATION)
+			return;
+		GenerateArray();
+		Clear(olc::BLACK);
+		m_ShouldDrawArray = true;
+		m_State = ApplicationState::ARRAY_GENERATION;
+	}
+	void SortButtonAction()
+	{
+		if (m_State != ApplicationState::ARRAY_GENERATION)
+			return;
+		m_Sort = SortingAlgorithmFactory::GetSortingAlgorithm(m_SortingAlgorithm);
+		m_State = ApplicationState::SORTING_ANIMATION;
+		m_ShouldDrawArray = true;
+		std::vector<int> copy(m_NumberArray);
+		m_AnimationVector = m_Sort->SortNumbers(copy);
+		m_AnimationIterator = m_AnimationVector.begin();
+	}
 	void GenerateArray()
 	{
 		m_NumberArray.clear();
-		// Generate random numbers array
 		for (size_t i = 0; i < NUM_ELEMENTS; i++)
 		{
 			std::random_device randomDevice;
@@ -161,8 +194,9 @@ private:
 	}
 	void DrawButton(const Button& button)
 	{
-		DrawRect(button.x, button.y, button.width, button.height, olc::DARK_GREEN);
-		DrawString(button.x + (button.width - button.text.length()*8)/2, button.y + button.height / 2 - 4, button.text);
+		FillRect(button.x, button.y, button.width, button.height, olc::GREY);
+		DrawRect(button.x, button.y, button.width, button.height, button.state == Button::State::CHECKED ? olc::DARK_GREEN : olc::BLUE );
+		DrawString(button.x + (button.width - button.text.length()*8)/2, button.y + button.height / 2 - 4, button.text, button.state == Button::State::CHECKED ? olc::DARK_GREEN : olc::BLUE);
 	}
 	void DrawNumbers(olc::Pixel color = olc::BLUE)
 	{
@@ -199,7 +233,7 @@ private:
 int main()
 {
 	SortingAlgorithms demo;
-	if (demo.Construct(1366, 768, 1, 1))
+	if (demo.Construct(1370, 768, 1, 1))
 		demo.Start();
 	return 0;
 }
